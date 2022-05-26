9dc6896e93f1ca273eda5a90d9888349637465a6
lucisortum <peli_ph17@hotmail.com>
2022-05-25 23:55:56 -0600

    agregando ejercicio 2

---
+++ EJERCICIOS/Ejercicio2/mensajeria.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string nombre;
+    cout << "Ingrese su nombre: ";
+    cin >> nombre;
+    cout << endl;
+    cout << "Me presento, mi nombre es " << nombre;
+    cout << endl;
+
+    return 0;
+}
